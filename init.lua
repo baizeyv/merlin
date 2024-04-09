807939c723dd49cdd0d61b0b86ce9f1efc64245c
baizeyv <baizeyv@gmail.com>
2024-04-09 22:49:28 +0800

    add plenary.nvim plugin

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- load options
 require("merlin.config.options").load_options()
+
+-- load keymaps (schedule)
 require("merlin.config.mappings").load_keymaps()
+
+-- load plugins
 require("merlin.core.plugin-loader").load()
