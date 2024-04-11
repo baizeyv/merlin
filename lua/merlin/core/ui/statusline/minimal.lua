3b0d8a943244cbb66343ecf2c99f356215c2174d
baizeyv <baizeyv@gmail.com>
2024-04-11 20:06:35 +0800

    arrange highlight group comment

--- lua/merlin/core/ui/statusline/minimal.lua
+++ lua/merlin/core/ui/statusline/minimal.lua
@@ -2,6 +2,55 @@ local rc = require("merlin.rc").ui.statusline
 local sep_style = rc.sep_style
 local utils = require("merlin.core.ui.statusline.utils")
 
+--
+-- 当前文件中存在的 highlight group
+-- STL_Sep_Right
+-- STL_Empty_Space
+-- STL_Normal_Mode
+-- STL_NTerminal_Mode
+-- STL_Visual_Mode
+-- STL_Insert_Mode
+-- STL_Terminal_Mode
+-- STL_Replace_Mode
+-- STL_Select_Mode
+-- STL_Command_Mode
+-- STL_Confirm_Mode
+-- STL_Normal_Mode_Sep
+-- STL_NTerminal_Mode_Sep
+-- STL_Visual_Mode_Sep
+-- STL_Insert_Mode_Sep
+-- STL_Terminal_Mode_Sep
+-- STL_Replace_Mode_Sep
+-- STL_Select_Mode_Sep
+-- STL_Command_Mode_Sep
+-- STL_Confirm_Mode_Sep
+-- STL_Normal_Mode_Text
+-- STL_NTerminal_Mode_Text
+-- STL_Visual_Mode_Text
+-- STL_Insert_Mode_Text
+-- STL_Terminal_Mode_Text
+-- STL_Replace_Mode_Text
+-- STL_Select_Mode_Text
+-- STL_Command_Mode_Text
+-- STL_Confirm_Mode_Text
+-- STL_File_Sep
+-- STL_File_Bg
+-- STL_File_Txt
+-- STL_Git_Icons
+-- STL_Cwd_Sep
+-- STL_Cwd_Bg
+-- STL_Cwd_Txt
+-- STL_Lsp
+-- STL_Lsp_Msg
+-- STL_Pos_Sep
+-- STL_Pos_Bg
+-- STL_Pos_Txt
+-- STL_Lsp_Error
+-- STL_Lsp_Warning
+-- STL_Lsp_Hints
+-- STL_Lsp_Info
+--
+
 sep_style = (sep_style ~= "round" and sep_style ~= "block") and "block" or sep_style
 
 local default_sep_icons = {
